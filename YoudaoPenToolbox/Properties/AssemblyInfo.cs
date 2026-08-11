1620b808eb7dfcda7a89802e382b4449248aee14
MIRROR <78773563+mirror2008@users.noreply.github.com>
2026-08-11 14:40:42 +0800

    Release v1.1.1: helper miniapp replaces ADB persist

--- YoudaoPenToolbox/Properties/AssemblyInfo.cs
+++ YoudaoPenToolbox/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-[assembly: AssemblyTitle("有道词典笔工具箱")]
-[assembly: AssemblyDescription("有道词典笔多设备管理工具箱")]
+[assembly: AssemblyTitle("鏈夐亾璇嶅吀绗斿伐鍏风")]
+[assembly: AssemblyDescription("鏈夐亾璇嶅吀绗斿璁惧绠＄悊宸ュ叿绠?)]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("YoudaoPenToolbox")]
@@ -12,5 +12,5 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
